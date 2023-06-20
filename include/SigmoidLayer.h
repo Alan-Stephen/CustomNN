8a1aca77691a60a7454ab4e2b4d29b8252499f2b
Alan-Stephen <stephenalan101@gmail.com>
2023-06-20 21:33:48 +0100

    Add Sigmoid Layer

--- include/SigmoidLayer.h
+++ include/SigmoidLayer.h
@@ -6,9 +6,21 @@
 #define CUSTOMNN_SIGMOIDLAYER_H
 
 #include "matrix.h"
+#include "Layer.h"
 
-class SigmoidLayer {
-    Matrix feedFoward(const Matrix &in);
+class SigmoidLayer : public Layer {
+public:
+    void updateGradients(Matrix &error, Matrix &previousLayerActivations) override;
+    void applyGradients() override;
+    void clearGradients() override;
+    Matrix layerOutput() override;
+    Matrix feedForward(const Matrix &in) override;
+    void randomizeParams() override;
+    Matrix getDerivitive(const Matrix &in) override;
+    void printLayer() const override;
+private:
+    double sigmoid(double in) const;
+    Matrix _output;
 };
 
 #endif //CUSTOMNN_SIGMOIDLAYER_H
